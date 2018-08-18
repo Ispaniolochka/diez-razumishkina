7f2b3012146b7b8e78939747be671f600c61709a
Ispaniolochka <olashmaker@rambler.ru>
2018-08-18 05:07:28 +0300

    Footer на десктопе и исправлены отступы контейнеров

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	
     var toggler = document.getElementById('toggler');
-toggler.addEventListener('click', mainNavVisibleToggle);
-function mainNavVisibleToggle(e) {
-  e.preventDefault();
-  toggler.classList.toggle('toggler--close');
-  document.getElementById('nav').classList.toggle('nav--visible');
+    toggler.addEventListener('click', mainNavVisibleToggle);
+    function mainNavVisibleToggle(e) {
+    e.preventDefault();
+    toggler.classList.toggle('toggler--close');
+    document.getElementById('nav').classList.toggle('nav--visible');
 }
-}
+});
