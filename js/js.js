b2d01b9a47572c51b8f73e26b219020defe0803a
Ispaniolochka <olashmaker@rambler.ru>
2018-08-20 14:54:35 +0300

    Макет готов к сдаче

--- js/js.js
+++ js/js.js
@@ -1,14 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$(".direction").selectize();
-	$(".button_burger").on("click", function(){
-        $(".button_burger").toggleClass("button_burger-open");
-        $(".burger").slideToggle();
-    
-	   })
+	$(".button_burger").on("click", function () {
+		$(".button_burger").toggleClass("button_burger-open");
+		$(".burger").slideToggle();
 	})
-$(document).ready(function(){
-$('.container_big_video').slick({
-	arrows: true,
+})
+ymaps.ready(init);
 
-});
-});
+function init() {
+	let myMap = new ymaps.Map("map", {
+		center: [59.945451, 30.276131],
+		zoom: 14
+	});
+	var myPlacemark = new ymaps.Placemark([59.945451, 30.276131], {
+		hintContent: 'Содержимое всплывающей подсказки',
+		balloonContent: 'Содержимое балуна'
+	}, {
+		iconLayout: 'default#image',
+		iconImageHref: 'img/geotag.png',
+		iconImageSize: [38, 49]
+	});
+	myMap.geoObjects.add(myPlacemark);
+}
